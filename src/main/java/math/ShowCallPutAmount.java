a25cec5d9a62f09d9d4e909b01136755dab417da
Vlad Vasile <vlad.vasile@dcsi.eu>
2018-02-20 15:17:43 +0200

    messages added

--- src/main/java/math/ShowCallPutAmount.java
+++ src/main/java/math/ShowCallPutAmount.java
@@ -14,6 +14,7 @@ public class ShowCallPutAmount {
 
 	public int registerNumber() {
 		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter buget:");
 		int buget = sc.nextInt();
 		sc.close();
 		return buget;
@@ -21,6 +22,7 @@ public class ShowCallPutAmount {
 
 	public void calculate(int buget) {
 		float[] x = { 1, (float) 2.2, (float) 4.8, (float) 10.5, 24, (float) 57.5 };
+		System.out.println("Amounts to Call/Put:");
 		for (float y : x) {
 			float result = (buget * y) / 100;
 			System.out.println(result);
