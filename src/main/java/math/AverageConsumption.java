42b81c60617ff7ee8aa04a549e51d85927065b92
Vlad Vasile <vlad.vasile@dcsi.eu>
2018-03-13 17:30:26 +0200

    refactored

--- src/main/java/math/AverageConsumption.java
+++ src/main/java/math/AverageConsumption.java
@@ -7,37 +7,20 @@ public class AverageConsumption {
   private static Scanner sc;
 
   public static void main(String[] args) {
-    calculateAvgConsumption(distanceTravelled(), moneySpent(), gasPrice());
+    
+    System.out.println("Enter distance travelled");
+    float distanceTravelled = scanFloat();
+    System.out.println("Enter money spent");
+    float moneySpent = scanFloat();
+    System.out.println("Enter gas price:");
+    float gasPrice = scanFloat();
+    calculateAvgConsumption(distanceTravelled, moneySpent, gasPrice);
   }
 
-  private static void calculateAvgConsumption(int distanceTravelled, int moneySpent, float price) {
-    float moneyPer100km = (moneySpent * 100) / distanceTravelled;
+  private static void calculateAvgConsumption(float distanceTravelled, float allMoneySpent, float price) {
+    float moneyPer100km = (allMoneySpent * 100) / distanceTravelled;
     float result = moneyPer100km / price;
     System.out.println("Your average consumption is: " + result);
-  }
-
-  private static float gasPrice() {
-    System.out.println("Enter gas price:");
-    float x = scanFloat();
-    return x;
-  }
-
-  private static int moneySpent() {
-    System.out.println("Enter money spent:");
-    int x = scanInt();
-    return x;
-  }
-
-  private static int distanceTravelled() {
-    System.out.println("Enter distance travelled:");
-    int x = scanInt();
-    return x;
-  }
-
-  private static int scanInt() {
-    sc = new Scanner(System.in);
-    int y = sc.nextInt();
-    return y;
   }
 
   private static float scanFloat() {
