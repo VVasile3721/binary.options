109f520d6ed3a4ab262563f8512c0bfc677dffa4
Vlad Vasile <vlad.vasile@dcsi.eu>
2018-10-22 10:37:31 +0300

    added lol files

--- src/main/java/math/MegaBytesConverter.java
+++ src/main/java/math/MegaBytesConverter.java
@@ -1,21 +1,41 @@
 package math;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class MegaBytesConverter {
 
   public static void main(String[] args) {
-    printMegaBytesAndKiloBytes(1025);
+    printMegaBytesAndKiloBytes(111523);
+    String testFileName = "/test.txt";
+    Path pathToFile = Paths.get("target" + testFileName);
+    System.out.println(sizeInKilobytes(pathToFile.toFile()));
+    System.out.println(filesize_in_Bytes(pathToFile.toFile()));
+  }
+
+  public static void printMegaBytesAndKiloBytes(long kiloBytes) {
+    long oneMegaByte = 1024;
+    long remainingKiloBytes = kiloBytes % oneMegaByte;
+    long megaBytes = (kiloBytes - remainingKiloBytes) / oneMegaByte;
+
+    if (kiloBytes < 0) {
+      System.out.println("Invalid Value");
+    } else {
+      System.out.println(kiloBytes + " KB = " + megaBytes + " MB and " + remainingKiloBytes + " KB");
+    }
+  }
+
+  public static long sizeInKilobytes(File file) {
+    long fileSizeInBytes = file.length();
+    long fileSizeInKB = fileSizeInBytes / 1024;
+    return fileSizeInBytes;
   }
   
-  public static void printMegaBytesAndKiloBytes(int kiloBytes){
-    int oneMegaByte = 1024;
-    int remainingKiloBytes = kiloBytes % oneMegaByte;
-    int megaBytes = (kiloBytes - remainingKiloBytes) / oneMegaByte;
-    
-    if(kiloBytes < 0){
-        System.out.println("Invalid Value");
-    } else {
-        System.out.println(kiloBytes + " KB = " + megaBytes + " MB and " + remainingKiloBytes + " KB");
-    }
+  private static String filesize_in_Bytes(File file) {
+    return file.length()+" bytes";
 }
-  
+
 }
